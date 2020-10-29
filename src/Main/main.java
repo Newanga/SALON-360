e1c6739c8480a85f8a0ff968daa5d9cb7435f4de
Newanga Wickramasinghe <newanga@live.com>
2020-10-29 23:58:05 +0530

    Add initial views of app along with the template views.

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,17 +2,23 @@ package Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../Views/Home.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Home");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+
         primaryStage.show();
     }
 
