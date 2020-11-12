13619821640eb1ef64097173430706050b2d90c9
ruxg97 <ruchira52@outlook.com>
2020-11-12 21:35:04 +0530

    Add Account Data Access Object

---
+++ src/data_access/AccountDAO.java
@@ -0,0 +1,81 @@
+package data_access;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import models.Account;
+import models.Employee;
+
+import java.sql.*;
+
+public class AccountDAO {
+
+    private Connection conn;
+    private PreparedStatement statement = null;
+    private ResultSet result = null;
+
+    public AccountDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public boolean UpdateAccount(Account model){
+        DataSource db = new DataSource();
+        try{
+            conn=db.getConnection();
+            final String sql="UPDATE Account SET Username=?,Password=? where Id=?;";
+            statement = conn.prepareStatement(sql);
+            statement.setString(1,model.getUsername());
+            statement.setString(2,model.getPassword());
+            statement.setInt(3,model.getId());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public ObservableList<Account> getAllAccounts() {
+        ObservableList<Account> accounts = FXCollections.observableArrayList();
+        DataSource db = new DataSource();
+
+
+        final String sql = "SELECT a.id,a.Username,a.Password,e.FirstName,e.LastName,es.Name as State ,er.Name as Role\n" +
+                "FROM account as a\n" +
+                "inner join employee as e\n" +
+                "on a.EmployeeId=e.id\n" +
+                "inner join employeestate as es\n" +
+                "on e.StateId=es.id\n" +
+                "inner join employeerole as er\n" +
+                "on e.RoleId=er.Id;";
+
+        try {
+            statement = conn.prepareStatement(sql);
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                Account acc = new Account();
+                acc.setId(result.getInt("Id"));
+                acc.setFirstName(result.getString("FirstName"));
+                acc.setLastName(result.getString("LastName"));
+                acc.setState(result.getString("State"));
+                acc.setRole(result.getString("Role"));
+                acc.setUsername(result.getString("Username"));
+                acc.setPassword(result.getString("Password"));
+                accounts.add(acc);
+            }
+            return accounts;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return accounts;
+
+    }
+
+    public void close() throws SQLException {
+        try {
+            ConnectionResources.close(result, statement, conn);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
