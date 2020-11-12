1103f7434d9113a9c87afc2572f7447c66920892
ruxg97 <ruchira52@outlook.com>
2020-11-12 21:29:32 +0530

    Add Validation for Employee

---
+++ src/validation/EmployeeFormValidation.java
@@ -0,0 +1,42 @@
+package validation;
+
+import models.Employee;
+
+public class EmployeeFormValidation {
+    public static boolean validateEmptyData(Employee model) {
+        if (
+                model.getFirstName().isEmpty() ||
+                        model.getLastName().isEmpty() ||
+                        model.getEmail().isEmpty() ||
+                        model.getAddress().isEmpty() ||
+                        model.getContactNo().isEmpty() ||
+                        model.getDob().toString() == null
+        )
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean validateContactNo(String contactNo) {
+        boolean valid;
+
+        try {
+            Integer.parseInt(contactNo);
+            valid = true;
+        } catch (NumberFormatException e) {
+            valid = false;
+        }
+
+        if (contactNo.length() == 10)
+            valid = true;
+        else
+            valid = false;
+
+        return valid;
+    }
+
+    public static boolean ValidateEmail(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+}
