5059a418ff57fd719983af7dc19768936d0d049d
ruxg97 <ruchira52@outlook.com>
2020-11-12 21:33:10 +0530

    Add Account Controller and Account Model

---
+++ src/controllers/AccountController.java
@@ -0,0 +1,361 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import data_access.*;
+import helpers.DialogMessages;
+import helpers.Export;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import models.Account;
+import validation.AccountFormValidation;
+import validation.EmployeeFormValidation;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AccountController implements Initializable {
+
+    @FXML
+    private StackPane stackpane;
+
+    @FXML
+    private JFXTextField tfSearchTerm;
+
+    @FXML
+    private TableView<Account> tvAccounts;
+
+    @FXML
+    private TableColumn<Account, Integer> colId;
+
+    @FXML
+    private TableColumn<Account, String> colFirstName;
+
+    @FXML
+    private TableColumn<Account, String> colLastName;
+
+    @FXML
+    private TableColumn<Account, String> colRole;
+
+    @FXML
+    private TableColumn<Account, String> colUsername;
+
+    @FXML
+    private TableColumn<Account, String> colPassword;
+
+    @FXML
+    private TableColumn<Account, String> colState;
+
+    @FXML
+    private JFXTextField tfId;
+
+    @FXML
+    private JFXTextField tfFirstName;
+
+    @FXML
+    private JFXTextField tfLastName;
+
+    @FXML
+    private JFXComboBox<String> cbRole;
+
+    @FXML
+    private JFXTextField tfUsername;
+
+    @FXML
+    private JFXTextField tfPassword;
+
+    @FXML
+    private JFXComboBox<String> cbState;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    private DataSource db;
+    private Connection conn;
+    private EmployeeRoleDAO employeeRoleDAO;
+    private EmployeeStateDAO employeeStateDAO;
+    private ObservableList<Account> accountList;
+    private AccountDAO accountDAO;
+    private Account accountModel = null;
+
+//Todo :DashBoard Data
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        InitialLoad();
+    }
+
+    private void InitialLoad() {
+        LoadAccountComboBoxData();
+        ShowAccounts();
+    }
+
+
+    private void LoadAccountComboBoxData() {
+        try {
+            cbState.setItems(FXCollections.observableArrayList(loadEmployeeStates()));
+            cbRole.setItems(FXCollections.observableArrayList(loadRoles()));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private List<String> loadRoles() {
+        List<String> list = FXCollections.observableArrayList();
+        try {
+
+            db = new DataSource();
+            conn = db.getConnection();
+            employeeRoleDAO = new EmployeeRoleDAO(conn);
+            list = employeeRoleDAO.getAllEmployeeRoleNames();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            //Close SQL Resources
+            if (employeeRoleDAO != null)
+                employeeRoleDAO.close();
+            ConnectionResources.close(conn);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    private List<String> loadEmployeeStates() {
+        List<String> list = FXCollections.observableArrayList();
+        try {
+
+            db = new DataSource();
+            conn = db.getConnection();
+            employeeStateDAO = new EmployeeStateDAO(conn);
+            list = employeeStateDAO.getAllEmployeeStateNames();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            //Close SQL Resources
+            if (employeeStateDAO != null)
+                employeeStateDAO.close();
+            ConnectionResources.close(conn);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    private void ShowAccounts() {
+        accountList = LoadAccountsFromDB();
+        colId.setCellValueFactory(new PropertyValueFactory<Account, Integer>("id"));
+        colFirstName.setCellValueFactory(new PropertyValueFactory<Account, String>("firstName"));
+        colLastName.setCellValueFactory(new PropertyValueFactory<Account, String>("lastName"));
+        colUsername.setCellValueFactory(new PropertyValueFactory<Account, String>("username"));
+        colPassword.setCellValueFactory(new PropertyValueFactory<Account, String>("password"));
+        colState.setCellValueFactory(new PropertyValueFactory<Account, String>("state"));
+        colRole.setCellValueFactory(new PropertyValueFactory<Account, String>("role"));
+        tvAccounts.setItems(accountList);
+    }
+
+    private ObservableList<Account> LoadAccountsFromDB() {
+        ObservableList<Account> list = FXCollections.observableArrayList();
+
+        try {
+            db = new DataSource();
+            conn = db.getConnection();
+            accountDAO = new AccountDAO(conn);
+            list = accountDAO.getAllAccounts();
+            return list;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                //closing connection resources
+                if (accountDAO != null)
+                    accountDAO.close();
+                if (conn != null)
+                    ConnectionResources.close(conn);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        return list;
+    }
+
+    public void clearSTextFieldsAndComboBoxes() {
+        tfId.clear();
+        tfFirstName.clear();
+        tfLastName.clear();
+        tfSearchTerm.clear();
+        tfUsername.clear();
+        tfPassword.clear();;
+        cbState.setValue(null);
+        cbRole.setValue(null);
+    }
+
+    public void btnUpdateClicked() throws SQLException {
+        DialogMessages dm = new DialogMessages(stackpane);
+        try{
+            db = new DataSource();
+            conn = db.getConnection();
+            accountDAO=new AccountDAO(conn);
+            accountModel = new Account();
+
+            accountModel.setId(Integer.parseInt(tfId.getText()));
+            accountModel.setFirstName(tfFirstName.getText());
+            accountModel.setLastName(tfLastName.getText());
+            accountModel.setState(cbState.getValue());
+            accountModel.setRole(cbRole.getValue());
+            accountModel.setUsername(tfUsername.getText());
+            accountModel.setPassword(tfPassword.getText());
+
+            //Check for empty data
+            boolean isDataEmpty= AccountFormValidation.validateEmptyData(accountModel);
+            if(isDataEmpty){
+                dm.EmptyDataInForm();
+                return;
+            }
+
+//            boolean isUsernameValid= AccountFormValidation.validateUsername(accountModel.getUsername());
+//            if(!isUsernameValid){
+//                dm.InvalidUsername();
+//                return;
+//            }
+
+//            boolean isPassswordValid= AccountFormValidation.validatePassword(accountModel.getUsername());
+//            if(!isPassswordValid){
+//                dm.InvalidPassword();
+//                return;
+//            }
+
+
+            Boolean result = accountDAO.UpdateAccount(accountModel);
+
+            if (result == true)
+                dm.UpdateSuccessDialogBox();
+            else
+                dm.UpdateFailedDialogBox();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        clearSTextFieldsAndComboBoxes();
+        InitialLoad();
+        btnUpdate.setDisable(true);
+        tfPassword.setDisable(true);
+        tfUsername.setDisable(true);
+
+        try{
+            //closing connection resources
+            if (accountDAO != null)
+                accountDAO.close();
+            if (conn != null)
+                ConnectionResources.close(conn);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void tvMouseClicked(MouseEvent event) throws SQLException {
+        Account model = null;
+
+        //check for a double click on table to load to object
+        if (event.getClickCount() == 1) {
+            model = tvAccounts.getSelectionModel().getSelectedItem();
+        } else {
+            return;
+        }
+
+        //validates whether the selected object(row) is null or not null
+        if (model == null) {
+            return;
+        } else {
+            cbState.setValue(model.getState());
+            cbRole.setValue(model.getRole());
+            tfId.setText(String.valueOf(model.getId()));
+            tfFirstName.setText(model.getFirstName());
+            tfLastName.setText(model.getLastName());
+            tfUsername.setText(model.getUsername());
+            tfPassword.setText(model.getPassword());
+            btnUpdate.setDisable(false);
+            tfUsername.setDisable(false);
+            tfPassword.setDisable(false);
+        }
+    }
+
+    public void btnKeyClicked(KeyEvent event) throws SQLException {
+
+        if (event.getCode() == KeyCode.ESCAPE) {
+            tvAccounts.getSelectionModel().clearSelection();
+            clearSTextFieldsAndComboBoxes();
+            btnUpdate.setDisable(true);
+            tfUsername.setDisable(true);
+            tfPassword.setDisable(true);
+            LoadAccountComboBoxData();
+        }
+
+    }
+
+    public void btnSExcelExportClicked(MouseEvent mouseEvent) {
+        Export ex = new Export(tvAccounts, stackpane);
+        ex.run();
+
+    }
+
+    public void SearchFunction() {
+        // Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<Account> filteredData = new FilteredList<>(accountList, b -> true);
+
+        // 2. Set the filter Predicate whenever the filter changes.
+        tfSearchTerm.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(Account -> {
+                // If filter text is empty, display all persons.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                // Compare name and description of every category with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (Account.getFirstName().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true; // Filter matches name.
+                }else if (String.valueOf(Account.getLastName()).indexOf(lowerCaseFilter) != -1) {
+                    return true; // Filter matches price.
+                } else if (String.valueOf(Account.getId()).indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                } else
+                    return false; // Does not match.
+            });
+        });
+
+        // 3. Wrap the FilteredList in a SortedList.
+        SortedList<Account> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        // 	  Otherwise, sorting the TableView would have no effect.
+        sortedData.comparatorProperty().bind(tvAccounts.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        tvAccounts.setItems(sortedData);
+
+    }
+}
---
+++ src/models/Account.java
@@ -0,0 +1,77 @@
+package models;
+
+public class Account {
+
+    private int Id;
+
+    private String firstName;
+
+    private  String lastName;
+
+    private String username;
+
+    private String password;
+
+    private String state;
+
+    private String role;
+
+    public Account() {
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
