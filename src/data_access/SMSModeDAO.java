e7d93b0a81f09e9a2a3d8a952100872f901678d6
Newanga Wickramasinghe <newanga@live.com>
2020-11-19 11:35:51 +0530

    Add SMS Mode Data Access class.

---
+++ src/data_access/SMSModeDAO.java
@@ -0,0 +1,61 @@
+package data_access;
+
+import javafx.collections.FXCollections;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SMSModeDAO {
+
+    private Connection conn;
+    private PreparedStatement statement = null;
+    private ResultSet result = null;
+
+    public SMSModeDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public List<String> getAllSMSModeNames() throws SQLException {
+        List<String> smsModeNames = FXCollections.observableArrayList();
+        final String sql = "SELECT Name FROM smsmode;";
+        try {
+            statement = conn.prepareStatement(sql);
+            result = statement.executeQuery();
+            while (result.next()) {
+                String name =  result.getString("Name");
+                smsModeNames.add(name);
+            }
+            return smsModeNames;
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+        }
+        return smsModeNames;
+    }
+
+    public int getSMSModeIdByName(String name) throws SQLException {
+        final String sql = "SELECT Id FROM smsmode WHERE Name=?";
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, name);
+            result= statement.executeQuery();
+            // Navigate to first row
+            result.absolute(1);
+            //Get first row data
+            int id = result.getInt("id");
+            return id;
+        } catch (SQLException throwables) {
+            return 0;
+        }
+    }
+
+    public void close() throws SQLException {
+        try {
+            ConnectionResources.close(result, statement, conn);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
